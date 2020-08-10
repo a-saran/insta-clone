81cfd70177d410aefcdc2101257a5b84106b6577
asaran97 <asaran234@gmail.com>
2020-08-10 23:13:36 +0530

    added post component

---
+++ src/component/post/Post.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./styles.scss";
+
+import Avatar from "@material-ui/core/Avatar";
+
+const Post = ({
+  post: {
+    userName = "a",
+    caption = "lorem",
+    imageUrl = "https://www.timeoutdubai.com/public/styles/full_img/public/images/2020/07/13/IMG-Dubai-UAE.jpg?itok=ZxMZvtVv"
+  }
+}) => {
+  return (
+    <div className="post">
+      <div className="post__header">
+        <Avatar className="post__avatar" alt="as" src="" />
+        <h3>post</h3>
+      </div>
+
+      <img src={imageUrl} alt={userName} className="post__image" />
+
+      <h4 className="post__text">
+        <strong>{userName}</strong>
+        {caption}
+      </h4>
+    </div>
+  );
+};
+
+export default Post;
